5dcf6d966fa948de1e05bf1c443cdf43a49c8a95
Jeremy Cortez <jeremycortez@dhcp-vl2041-51878.eduroam.cornell.edu>
2026-02-27 12:12:50 -0500

    Change url path for deleting a User

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.get('/getAllUsers', authenticateToken, getAllUsers); // GET /api/middleware/getAllUsers
 router.get('/getUserById', authenticateToken, getUserById); // GET /api/middleware/getUserById?id=123
-router.delete('/:id', authenticateToken, deleteUserById); // DELETE /api/middleware/:id
+router.delete('/deleteUser/:id', authenticateToken, deleteUserById); // DELETE /api/middleware/:id
 
 export default router;
