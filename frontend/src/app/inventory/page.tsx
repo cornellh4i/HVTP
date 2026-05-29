06b285a085afac3a76457f417920251db99acced
Dhikshika Cherivirala <dhikshikac@Dhikshikas-MacBook-Pro.local>
2026-05-29 17:45:48 -0400

    redesigned the add lot btn, toggle btn, search bar, and itemCard

--- frontend/src/app/inventory/page.tsx
+++ frontend/src/app/inventory/page.tsx
@@ -88,7 +88,7 @@ export default function InventoryPage() {
                   mode === "view"
                     ? styles.viewToggleButtonActive
                     : styles.viewToggleButtonInactive
-                }`}
+                } rounded-l-lg`}
               >
                 <LayoutGrid size={16} />
               </button>
@@ -98,7 +98,7 @@ export default function InventoryPage() {
                   mode === "list"
                     ? styles.viewToggleButtonActive
                     : styles.viewToggleButtonInactive
-                }`}
+                } rounded-r-lg -ml-px`}
               >
                 <AlignJustify size={16} />
               </button>
